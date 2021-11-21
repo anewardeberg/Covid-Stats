381d640f8891b3adfed452ec5c98bcdf1e53395f
anewardeberg <anewardeberg@gmail.com>
2021-11-21 13:34:35 +0100

    file structure and config + some work on button

---
+++ app/screens/Choices.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Choices() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
