e037a102088b74fca727a6474256c4ccbc3fe50f
anewardeberg <anewardeberg@gmail.com>
2021-11-27 13:43:57 +0100

    implemented graph component on detail screen

--- app/screens/Detail.tsx
+++ app/screens/Detail.tsx
@@ -1,9 +1,10 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { RootStackParamList } from "../../App";
 import CovidApi from "../../CovidApi";
 import Flag from "../components/Flag";
+import Graph from "../components/Graph";
 import Heading from "../components/Heading";
 import Statistics from "../components/Statistics";
 import colors from "../config/colors";
@@ -24,6 +25,30 @@ export default function Detail(
   const [countryCode, setCountryCode] = useState("");
   const [deaths, setDeaths] = useState(0);
   const [recovered, setRecovered] = useState(0);
+  const [labels, setLabels] = useState({ data: [], loading: true });
+  const [data, setData] = useState({ data: [], loading: true });
+
+  useEffect(() => {
+    getCovidStats();
+    getCovidTimeSeriesData();
+  }, []);
+
+  async function getCovidTimeSeriesData() {
+    const covidTimeSeriesData = await CovidApi.getCovidTimeSeriesDataForCountry(
+      country
+    );
+    // https://github.com/indiespirit/react-native-chart-kit/issues/237
+    setLabels({
+      data: Object.keys(covidTimeSeriesData.timeline.cases) as never,
+      loading: false,
+    });
+    setData({
+      data: Object.values(covidTimeSeriesData.timeline.cases),
+      loading: false,
+    });
+    console.log(labels);
+    console.log(data);
+  }
 
   async function getCovidStats() {
     const countryCovidStats = await CovidApi.getCovidStatForCountry(country);
@@ -34,12 +59,14 @@ export default function Detail(
     setRecovered(countryCovidStats.recovered);
     setFlagUri(countryCovidStats.countryInfo.flag);
   }
-  getCovidStats();
   if (pageType == "infections") {
     return (
       <View style={styles.container}>
         <Heading text={countryCode} subtitle={countryName} type="detail" />
         <Statistics cases={cases} deaths={deaths} recovered={recovered} />
+        {data.loading ? null : (
+          <Graph labels={labels.data as never} data={data.data as never} />
+        )}
         <Flag uri={flagUri} type="detail" />
       </View>
     );
