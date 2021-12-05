a3f6f028080a52568ed4b2418926036d3db9f76b
anewardeberg <anewardeberg@gmail.com>
2021-12-05 21:50:22 +0100

    added react import statement

--- app/screens/Landing.tsx
+++ app/screens/Landing.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { StyleSheet, View } from "react-native";
 import Button from "../components/Exports";
